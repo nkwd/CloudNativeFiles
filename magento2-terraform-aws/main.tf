c3d8c64324fa12271b5c72183c19681ec37607fc
Xiaochuan Wang <nkwdwxc@gmail.com>
2018-09-22 09:10:55 -0400

    Added Magento2 WIP tf script.

---
+++ magento2-terraform-aws/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region     = "${var.aws_region}"
+}
+
+resource "aws_key_pair" "publicaccesskey" {
+    key_name = "admins-public-ssh-key"
+    public_key = ""
+}
+
+resource "aws_vpc" "default" {
+    cidr_block = "${var.aws_vpc_cidr}"
+    enable_dns_hostnames = true
+    tags {
+        Name = "magento2"
+        group = "mage-vpc"
+    }
+}
